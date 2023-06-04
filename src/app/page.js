0c74d29882b83e236cb10c183176bdf093bc3ef5
didyouwin07 <vishwasbordia@outlook.com>
2023-06-04 14:34:46 +0530

    Add mission page

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Header from "@/components/header/Header";
 import Button from "@/components/shared/Button";
-import { INTRO_TEXT_PRI, INTRO_TEXT_SEC, INTRO_TEXT_TER } from "@/utils/Constants";
+import { INTRO_TEXT_PRI, INTRO_TEXT_SEC, INTRO_TEXT_TER, MISSION_TEXT_PRI, MISSION_TEXT_SEC } from "@/utils/Constants";
 import Image from "next/image";
 import styles from "./page.module.css";
 
@@ -18,7 +18,15 @@ export default function Home() {
           <Button title="GET STARTED" width="60vw" height="5vh" />
         </div>
       </div>
-      <div className={styles.missionContainer}></div>
+      <div className={styles.missionContainer}>
+        <div className={styles.missionPrimaryText}>{MISSION_TEXT_PRI}</div>
+        <div className={styles.missionSecondaryText}>
+          <div className={styles.missionLine1}>Our mission is to build an</div>  
+          <div className={styles.missionLine2}>independent credible</div>  
+          <div className={styles.missionLine1}>media company, which tell</div>  
+          <div className={styles.missionLine2}>stories that matter.</div>  
+        </div>
+      </div>
     </main>
   );
 }
--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,3 +1,5 @@
 export const INTRO_TEXT_PRI = `Context is everything.`
 export const INTRO_TEXT_SEC = `Only read stories that matter.`
 export const INTRO_TEXT_TER = `Join Now and Start Reading for FREE.`
+export const MISSION_TEXT_PRI = `Our journalism`
+export const MISSION_TEXT_SEC = `Our mission is to build an independent credible media company, which tells stories that matter.`
