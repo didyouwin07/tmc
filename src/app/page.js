f0e1427b91790657bd93d42a8ec6a4d63c28d280
didyouwin07 <vishwasbordia@outlook.com>
2023-06-04 16:08:37 +0530

    Add why choose TMC section

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,30 @@
 import Header from "@/components/header/Header";
 import Button from "@/components/shared/Button";
-import { INTRO_TEXT_PRI, INTRO_TEXT_SEC, INTRO_TEXT_TER, MISSION_TEXT_PRI, MISSION_TEXT_SEC } from "@/utils/Constants";
+import {
+  INTRO_TEXT_PRI,
+  INTRO_TEXT_SEC,
+  INTRO_TEXT_TER,
+  MISSION_TEXT_PRI,
+  MISSION_TEXT_SEC,
+  WHY_TMC,
+} from "@/utils/Constants";
 import Image from "next/image";
 import styles from "./page.module.css";
+
+const WhyTMC = (props) => {
+  const { icon, primaryText, secondaryText } = props;
+  return (
+    <div className={styles.whyTMCComponent}>
+      <div className={styles.whyTMCHeader}>
+        <div className={styles.whyTMCIcon}>
+          <img src={icon} alt={primaryText} />
+        </div>
+        <div className={styles.whyTMCPrimary}>{primaryText}</div>
+      </div>
+      <div className={styles.whyTMCSecondary}>{secondaryText}</div>
+    </div>
+  );
+};
 
 export default function Home() {
   return (
@@ -21,11 +43,21 @@ export default function Home() {
       <div className={styles.missionContainer}>
         <div className={styles.missionPrimaryText}>{MISSION_TEXT_PRI}</div>
         <div className={styles.missionSecondaryText}>
-          <div className={styles.missionLine1}>Our mission is to build an</div>  
-          <div className={styles.missionLine2}>independent credible</div>  
-          <div className={styles.missionLine1}>media company, which tell</div>  
-          <div className={styles.missionLine2}>stories that matter.</div>  
+          <div className={styles.missionLine1}>Our mission is to build an</div>
+          <div className={styles.missionLine2}>independent credible</div>
+          <div className={styles.missionLine1}>media company, which tell</div>
+          <div className={styles.missionLine2}>stories that matter.</div>
         </div>
+      </div>
+      <div className={styles.whyTMCContainer}>
+        <div className={styles.whyTMCHeadingText}>Why choose TMC?</div>
+        {WHY_TMC.map((reason) => (
+          <WhyTMC
+            icon={reason.icon}
+            primaryText={reason.textPri}
+            secondaryText={reason.textSec}
+          />
+        ))}
       </div>
     </main>
   );
--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,5 +1,27 @@
-export const INTRO_TEXT_PRI = `Context is everything.`
-export const INTRO_TEXT_SEC = `Only read stories that matter.`
-export const INTRO_TEXT_TER = `Join Now and Start Reading for FREE.`
-export const MISSION_TEXT_PRI = `Our journalism`
-export const MISSION_TEXT_SEC = `Our mission is to build an independent credible media company, which tells stories that matter.`
+export const INTRO_TEXT_PRI = `Context is everything.`;
+export const INTRO_TEXT_SEC = `Only read stories that matter.`;
+export const INTRO_TEXT_TER = `Join Now and Start Reading for FREE.`;
+export const MISSION_TEXT_PRI = `Our journalism`;
+export const MISSION_TEXT_SEC = `Our mission is to build an independent credible media company, which tells stories that matter.`;
+export const WHY_TMC = [
+  {
+    icon: "/icons/why-tmc-1.svg",
+    textPri: `Information and analysis you won’t find anywhere else`,
+    textSec: `We publish the best writing in India across three categories—internet, business and chaos.`,
+  },
+  {
+    icon: "/icons/why-tmc-2.svg",
+    textPri: `Independent and unbiased journalism`,
+    textSec: `We are building a credible media company which tells stories that matter. Paid for by the readers..`,
+  },
+  {
+    icon: "/icons/why-tmc-3.svg",
+    textPri: `Premium newsletters`,
+    textSec: `Read what is new in big tech, corporate governance and the world, in general from some of the top writers in the country.`,
+  },
+  {
+    icon: "/icons/why-tmc-4.svg",
+    textPri: `TMC Dialogues`,
+    textSec: `Be a part of exclusive The Morning Context community.`,
+  },
+];
