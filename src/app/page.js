25a53a9022190dbc53e6874f537cca115a9ca54d
didyouwin07 <vishwasbordia@outlook.com>
2023-06-05 09:19:47 +0530

    Add message

--- src/app/page.js
+++ src/app/page.js
@@ -67,91 +67,101 @@ const CollectionCard = (props) => {
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.headerContainer}>
-        <Header />
+    <>
+      <div className={styles.message}>
+        Open in mobile view for the current version
       </div>
-      <div className={styles.introContainer}>
-        <div className={styles.introTextPri}>{INTRO_TEXT_PRI}</div>
-        <div className={styles.introTextSec}>{INTRO_TEXT_SEC}</div>
-        <div className={styles.introTextTer}>{INTRO_TEXT_TER}</div>
-        <div className={styles.getStartedButton}>
-          <Button title="GET STARTED" width="60vw" height="5vh" />
+      <main className={styles.main}>
+        <div className={styles.headerContainer}>
+          <Header />
         </div>
-      </div>
-      <div className={styles.missionContainer}>
-        <div className={styles.missionPrimaryText}>{MISSION_TEXT_PRI}</div>
-        <div className={styles.missionSecondaryText}>
-          <div className={styles.missionLine1}>Our mission is to build an</div>
-          <div className={styles.missionLine2}>independent credible</div>
-          <div className={styles.missionLine1}>media company, which tell</div>
-          <div className={styles.missionLine2}>stories that matter.</div>
+        <div className={styles.introContainer}>
+          <div className={styles.introTextPri}>{INTRO_TEXT_PRI}</div>
+          <div className={styles.introTextSec}>{INTRO_TEXT_SEC}</div>
+          <div className={styles.introTextTer}>{INTRO_TEXT_TER}</div>
+          <div className={styles.getStartedButton}>
+            <Button title="GET STARTED" width="60vw" height="5vh" />
+          </div>
         </div>
-      </div>
-      <div className={styles.whyTMCContainer}>
-        <div className={styles.whyTMCHeadingText}>Why choose TMC?</div>
-        {WHY_TMC.map((reason) => (
-          <WhyTMC
-            icon={reason.icon}
-            primaryText={reason.textPri}
-            secondaryText={reason.textSec}
-          />
-        ))}
-      </div>
-      <div className={styles.getStartedSection}>
-        <div className={styles.getStartedHeading}>
-          Get access to our Starter Collection by signing up for FREE.
+        <div className={styles.missionContainer}>
+          <div className={styles.missionPrimaryText}>{MISSION_TEXT_PRI}</div>
+          <div className={styles.missionSecondaryText}>
+            <div className={styles.missionLine1}>
+              Our mission is to build an
+            </div>
+            <div className={styles.missionLine2}>independent credible</div>
+            <div className={styles.missionLine1}>media company, which tell</div>
+            <div className={styles.missionLine2}>stories that matter.</div>
+          </div>
         </div>
-        <div className={styles.collectionCarouselContainer}>
-          {STARTER_COLLECTION.map((item) => (
-            <CollectionCard {...item} />
+        <div className={styles.whyTMCContainer}>
+          <div className={styles.whyTMCHeadingText}>Why choose TMC?</div>
+          {WHY_TMC.map((reason) => (
+            <WhyTMC
+              icon={reason.icon}
+              primaryText={reason.textPri}
+              secondaryText={reason.textSec}
+            />
           ))}
         </div>
-        <div className={styles.signUpNowSection}>
-          <div className={styles.signUpNowHeading}>
-            Sign up now to access the Starter Collection
+        <div className={styles.getStartedSection}>
+          <div className={styles.getStartedHeading}>
+            Get access to our Starter Collection by signing up for FREE.
           </div>
-          <div className={styles.starterCreditDetails}>
-            <div className={styles.creditDetailsInnerContainer}>
-              <div className={styles.creditsTitle}>Free 10 Starter Credits</div>
-              <div className={styles.creditBenefits}>
-                <div className={styles.creditBenefit}>
-                  <div className={styles.greenTick}>
-                    <img src="/icons/green-tick.svg" alt="Green Tick" />
+          <div className={styles.collectionCarouselContainer}>
+            {STARTER_COLLECTION.map((item) => (
+              <CollectionCard {...item} />
+            ))}
+          </div>
+          <div className={styles.signUpNowSection}>
+            <div className={styles.signUpNowHeading}>
+              Sign up now to access the Starter Collection
+            </div>
+            <div className={styles.starterCreditDetails}>
+              <div className={styles.creditDetailsInnerContainer}>
+                <div className={styles.creditsTitle}>
+                  Free 10 Starter Credits
+                </div>
+                <div className={styles.creditBenefits}>
+                  <div className={styles.creditBenefit}>
+                    <div className={styles.greenTick}>
+                      <img src="/icons/green-tick.svg" alt="Green Tick" />
+                    </div>
+                    <div className={styles.benefit}>
+                      Get FREE 10 Starter Credits
+                    </div>
                   </div>
-                  <div className={styles.benefit}>
-                    Get FREE 10 Starter Credits
+                  <div className={styles.creditBenefit}>
+                    <div className={styles.greenTick}>
+                      <img src="/icons/green-tick.svg" alt="Green Tick" />
+                    </div>
+                    <div className={styles.benefit}>
+                      Use Starter Credits to unlock free stories, newsletters
+                      and long reads
+                    </div>
+                  </div>
+                  <div className={styles.creditBenefit}>
+                    <div className={styles.greenTick}>
+                      <img src="/icons/green-tick.svg" alt="Green Tick" />
+                    </div>
+                    <div className={styles.benefit}>
+                      Starter Credits{" "}
+                      <span className={styles.bold}>do not expire</span> – use
+                      them as on stories of your choice
+                    </div>
                   </div>
                 </div>
-                <div className={styles.creditBenefit}>
-                  <div className={styles.greenTick}>
-                    <img src="/icons/green-tick.svg" alt="Green Tick" />
-                  </div>
-                  <div className={styles.benefit}>
-                    Use Starter Credits to unlock free stories, newsletters and
-                    long reads
-                  </div>
+                <div className={styles.signUpButton}>
+                  <Button title="SIGN UP FOR FREE" width="70%" height="6vh" />
                 </div>
-                <div className={styles.creditBenefit}>
-                  <div className={styles.greenTick}>
-                    <img src="/icons/green-tick.svg" alt="Green Tick" />
-                  </div>
-                  <div className={styles.benefit}>
-                    Starter Credits <span className={styles.bold}>do not expire</span> – use them as on stories of
-                    your choice
-                  </div>
-                </div>
-              </div>
-              <div className={styles.signUpButton}>
-                <Button title='SIGN UP FOR FREE' width="70%" height="6vh" />
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className={styles.footerContainer}>
-        <Footer />
-      </div>
-    </main>
+        <div className={styles.footerContainer}>
+          <Footer />
+        </div>
+      </main>
+    </>
   );
 }
